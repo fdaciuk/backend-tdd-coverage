cbf5ff823e27d36fa2931ce40b11afee6d1a4709
Fernando Daciuk <f.daciuk@gmail.com>
2015-09-11 20:24:38 -0300

    Add es6 style

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var gulp = require( 'gulp' );
-var mocha = require( 'gulp-mocha' );
-var istanbul = require( 'gulp-istanbul' );
-var exec = require( 'child_process' ).exec;
+import gulp from 'gulp';
+import mocha from 'gulp-mocha';
+import istanbul from 'gulp-istanbul';
 
-var testFiles = 'test/**/*.js';
-var srcFiles = 'src/**/*.js';
-var allFiles = [ testFiles, srcFiles ];
+const testFiles = 'test/**/*.js';
+const srcFiles = 'src/**/*.js';
+const allFiles = [testFiles, srcFiles];
 
-gulp.task( 'test', function( done ) {
-  gulp.src( srcFiles )
-    .pipe( istanbul() )
-    .pipe( istanbul.hookRequire() )
-    .on( 'finish', function() {
-      gulp.src( allFiles )
-      .pipe( mocha() )
-      .on( 'error', done )
-      .pipe( istanbul.writeReports() )
-      .on( 'end', done );
+gulp.task('test', done => {
+  gulp.src(srcFiles)
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', () => {
+      gulp.src(allFiles)
+      .pipe(mocha())
+      .on('error', done)
+      .pipe(istanbul.writeReports())
+      .on('end', done);
     });
 });
 
-gulp.task( 'default', [ 'test' ], function() {
-  gulp.watch( allFiles, [ 'test' ] );
-});
+gulp.task(
+  'default', ['test'],
+  () => gulp.watch(allFiles, ['test'])
+);
